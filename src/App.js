5f96951f97a66942ae694367cc40125a55c2cf40
jobayrulhasan <jobayerdpi11@gmail.com>
2020-08-23 18:36:02 +0600

    destracture usestate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const mobilePhones = ['Modern','Less Modern','Poor Modern','Updated']
   return (
     <div className="App">
-      {/* this is the another way 
-      <Nayok name = 'Mobile phone'></Nayok>
-      <Nayok name = 'Cell phone'></Nayok>
-      <Nayok name = 'Tele phone'></Nayok>
-      <Nayok name = 'Modern phone'></Nayok> */}
-
-      <Nayok name = {mobilePhones[3]}></Nayok>
-      <Nayok name = {mobilePhones[0]}></Nayok>
-      <Nayok name = {mobilePhones[1]}></Nayok>
-      <Nayok name = {mobilePhones[2]}></Nayok>
-     
+      <MovieCounter></MovieCounter>
+      <MovieCounter></MovieCounter>
+      <MovieCounter></MovieCounter>
+      <MovieCounter></MovieCounter>
+  
       <header className="App-header">
-      
-      <h1>commit</h1>
-      
+      <h1>Buttom portion react</h1>
       </header>
     </div>
   );
 }
-function Nayok(props) {
-  // Any kinds of name is valid in the position of props.it's action is called props only
-console.log(props)
+function MovieCounter() {
+  const [count , setCount] =  useState(0)
+  const handleClick = () => setCount(count + 1)
+
   const headStyle = {
     border : '2px solid red',
     padding: '10px',
@@ -36,11 +28,15 @@ console.log(props)
   }
   return (
     <div style ={headStyle}>
-      <h1>Name of the head: {props.name}</h1>
-      <h3>Heading of the page</h3>
-      <h5>little portion</h5>
+      <button onClick = {handleClick}>Add movies</button>
+      <h5>Number of movies: {count} </h5>
+      <MoviesDisplay movies = {count + 10}></MoviesDisplay>
+      <MoviesDisplay movies = {count -1 }></MoviesDisplay>  
     </div>
   );
+}
+function MoviesDisplay(props) {
+  return <h4>Movies i had watch: {props.movies} </h4>
 }
 
 export default App;
